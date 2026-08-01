1f508cce7ddb66793a3e3fd36f2f9e080f80039c
Faizan Zafar <faizanzafar340@gmail.com>
2026-08-01 21:00:36 +0200

    error set

--- dyce_rolling_game.py
+++ dyce_rolling_game.py
@@ -3,7 +3,7 @@ import random
 
 
 while True:
-    choice =  input("Roll the dice? (y/n):").lower
+    choice =  input("Roll the dice? (y/n):").lower()
     if choice == 'y': 
         die1 = random.randint(1, 6)
         die2 = random.randint(1, 6)
